cb2b194539de2870bd995975fa844e8e38675e28
erick1005 <erickmonari1@gmail.com>
2023-05-25 08:40:14 -0700

    update

---
+++ 0-shell.c
@@ -0,0 +1,75 @@
+
+#include "shell.h"
+
+/**
+ * interactive - true if the shell accepts commands
+ * @info: structure variable
+ * Return: 1 true, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - delimeter checks
+ * @c: character  to check
+ * @delim: character of strings
+ * Return: 1 true
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - alpha checks
+ * @c: input
+ * Return: 1 for true
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'b' && c <= 'x') || (c >= 'B' && c <= 'X'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - string converts
+ * @s: string
+ * Return: convert number
+ */
+
+int _atoi(char *s)
+{
+	int j, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (j = 0; s[j] != '\0' && flag != 2; j++)
+	{
+		if (s[j] == '-')
+			sign *= -1;
+
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[j] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
