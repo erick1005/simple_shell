84c9cc860daeb9187a04cc45291972f735766409
mwihoti <danielmwihoti@gmail.com>
2023-05-25 19:52:13 -0700

    shell

---
+++ 3-shell.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * cmyenv - current environmt
+ * @info: variable struct.
+ * Return: 0 for success
+ */
+int cmyenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * getenva - environmt variable
+ * @info: argument struct.
+ * @name:input
+ * Return: pointer
+ */
+char *getenva(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *q;
+
+	while (node)
+	{
+		q = starts_with(node->str, name);
+		if (q && *q)
+			return (q);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * mysetenvi -  customized variable
+ * @info: struct arguments.
+ *  Return: Always 0 success
+ */
+int mysetenvi(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _dmyunsetenv - deletes variable
+ * @info: argument struct.
+ * Return: success 0
+ */
+int _dmyunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_envlist - entry
+ * @info: argument structure
+ * Return: 0 for success
+ */
+int populate_envlist(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+
+	info->env = node;
+	return (0);
+}
---
+++ 4-shell.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+/**
+ * _eputsd - displays characters
+ * @str: input
+ * Return: void
+ */
+void _eputsd(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;	}
+}
+
+/**
+ *eputchar - displays char r to stderr
+ * @r: input
+ * Return: success 1.
+ */
+int eputchar(char r)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (r == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (r != BUF_FLUSH)
+		buf[i++] = r;
+	return (1);
+}
+
+/**
+ * put_fd - entry
+ * @r: character to print
+ * @fd: filedescriptor
+ * Return: success 1.
+ */
+int put_fd(char r, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (r == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (r != BUF_FLUSH)
+		buf[i++] = r;
+	return (1);
+}
+
+/**
+ * _edputsfd - display input string
+ * @str: input
+ * @fd: filedescriptor
+ * Return: int
+ */
+int _edputsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += put_fd(*str++, fd);
+	}
+	return (i);
+}
